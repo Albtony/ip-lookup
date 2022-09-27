a79b03c359f91df8f7554646a7651ba40bc7fda8
albtony <albtony21@gmail.com>
2022-09-27 20:16:44 +0700

    feat: web find user's IP onload

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// import { $ } from "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js";
+window.findUserIP = async () => {
+    const response = await fetch('https://api64.ipify.org?format=json').catch((err) => {
+        console.warn(err);
+        return;
+    });
+    const json = await response.json();
+    const ipv4 = json.ip;
+
+    let elemIpv4 = document.getElementById('ipv4');
+    elemIpv4.innerHTML = `Your current IP is ${ipv4}`;
+}
 
 window.hi = ()=>{
     alert("hi")
