2dae922cdeaa5df93d8b1403dc8b2aa6653c0dc3
albtony <albtony21@gmail.com>
2022-09-28 13:08:33 +0700

    add hardcoded map

--- main.js
+++ main.js
@@ -12,11 +12,40 @@ window.findUserIP = async () => {
 
 window.findIPInfo = async () => {
     const ip = document.getElementById('addressInput').values;
-    if(!/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(ip)) return;
+    // if(!/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(ip)) return;
     const uri = `http://www.geoplugin.net/json.gp?ip=${ip}`;
     const response = await fetch(uri).catch((err) => {
         console.warn(err);
         return;
     });
     const json = await response.json();
+    createMap(json.geoplugin_longitude, json.geoplugin_latitude);
+}
+
+window.createMap = (longitude, latitude) => {
+    console.log(longitude); // 110.5958
+    console.log(latitude);  // -7.5331
+    const view = new ol.View({
+        // center: ol.proj.fromLonLat([longitude, latitude]),
+        center: ol.proj.fromLonLat([110.5958, -7.5331]),
+        zoom: 12
+    });
+    const tile = new ol.layer.Tile({
+        source: new ol.source.OSM()
+    });
+    const marker = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: [
+                new ol.Feature({
+                    // geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude]))
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([110.5958, -7.5331]))
+                })
+            ]
+        })
+    });
+    let map = new ol.Map({
+        target: 'map',
+        layers: [tile, marker],
+        view: view
+    });
 }
