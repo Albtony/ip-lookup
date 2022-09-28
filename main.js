d2cbd3a80431439116497f0222e758998c8826f8
albtony <albtony21@gmail.com>
2022-09-28 11:44:42 +0700

    successfully get ip info

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ window.findUserIP = async () => {
     let elemIpv4 = document.getElementById('ipv4');
     elemIpv4.innerHTML = `Your current IP is ${ipv4}`;
 }
+
+window.findIPInfo = async () => {
+    const ip = document.getElementById('addressInput').values;
+    if(!/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(ip)) return;
+    const uri = `http://www.geoplugin.net/json.gp?ip=${ip}`;
+    const response = await fetch(uri).catch((err) => {
+        console.warn(err);
+        return;
+    });
+    const json = await response.json();
+}
